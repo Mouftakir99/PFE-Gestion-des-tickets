4ff3a8419324bc1c067125a82bec4801000abf18
mouftakir99 <mouftakiraiman@gmail.com>
2022-09-06 20:07:01 +0100

    zz
    Signed-off-by: mouftakir99 <mouftakiraiman@gmail.com>

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt("password"),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'Agent 1',
+                'email'          => 'agent1@agent1.com',
+                'password'       => bcrypt("password"),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 3,
+                'name'           => 'Agent 2',
+                'email'          => 'agent2@agent2.com',
+                'password'       => bcrypt("password"),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 4,
+                'name'           => 'Agent 3',
+                'email'          => 'agent3@agent3.com',
+                'password'       => bcrypt("password"),
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
